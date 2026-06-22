7eed6dc6b43aec2894c56dacb3f2018d080b048d
Chintan <opentheatre20@gmail.com>
2026-06-22 13:34:33 +0530

    Revert "feat: add DRM key and m3u8 proxy endpoints to fix CORS issue"
    This reverts commit 2d3fd66ac6f7c704f643e8d21327d2d3fa6df18c.

--- backend/src/routes/videoRoutes.ts
+++ backend/src/routes/videoRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getStreamForMovie, updateWatchTime, proxyM3u8, proxyDrmKey } from '../controllers/videoController';
+import { getStreamForMovie, updateWatchTime } from '../controllers/videoController';
 import { protect } from '../middlewares/authMiddleware';
 import { requireActiveRental } from '../middlewares/rentalMiddleware';
 
@@ -7,16 +7,6 @@ const router = express.Router();
 
 // Enforcing protect & requireActiveRental
 router.get('/:movieId/stream', protect, requireActiveRental, getStreamForMovie);
-router.post('/:movieId/history', protect, updateWatchTime);
-
-// DRM key and m3u8 proxy routes (public, auth is handled by Bunny token)
-router.options('/:videoId/drm-key/:keyId', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  res.sendStatus(204);
-});
-router.get('/:videoId/drm-key/:keyId', proxyDrmKey);
-router.get('/:videoId/playlist.m3u8', protect, requireActiveRental, proxyM3u8);
+router.post('/:movieId/history', protect, updateWatchTime); 
 
 export default router;
